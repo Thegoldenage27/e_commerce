6f03992daa2605f26043ef704b64ca1e1d39510d
Thegoldenage27 <molinaf59@gmail.com>
2023-03-29 14:36:09 -0600

    Prueba2_08_adaptando_nuestra_pantilla

--- index.php
+++ index.php
@@ -172,7 +172,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- /.content-wrapper -->
 
   <!-- Control Sidebar -->
-  
+   
   <!-- /.control-sidebar -->
 
   <!-- Main Footer -->
