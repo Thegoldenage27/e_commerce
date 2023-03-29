b7bbc94dd02c7db8253d7374cb351e0c15fa4f0d
Thegoldenage27 <molinaf59@gmail.com>
2023-03-29 01:30:49 -0600

    Prueba2_04_como_validar_el_usuario_y_contraseña

---
+++ app/controllers/login/ingreso.php
@@ -0,0 +1,21 @@
+<?php
+include ('../../config.php');
+$email = $_POST['email'];
+$password_user= $_POST['password_user'];
+$contador =0;
+$sql = "SELECT * FROM clientes WHERE email = '$email' AND password_user = '$password_user'";
+$query = $pdo->prepare($sql);
+$query->execute();
+
+$usuarios = $query->fetchAll(PDO::FETCH_ASSOC);
+foreach ($usuarios as $usuario) {
+    $contador = $contador +1;
+    $email_tabla = $usuario['email'];
+    $nombres = $usuario['nombre'];
+}
+
+if($contador == 0){
+    echo "Datos incorrectos, por favor vuelve a intentarlo";
+}else{
+    echo "Datos correctos";
+}
